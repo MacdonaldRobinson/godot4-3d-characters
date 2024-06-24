189d01223ae31f92812aa3929c125f55fc64d141
MacdonaldRobinson <maocnly2001@gmail.com>
2024-06-23 18:56:37 -0600

    Updated hud character

--- ui/action-bar/hud/hud.gd
+++ ui/action-bar/hud/hud.gd
@@ -9,6 +9,7 @@ extends Control
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
+	character.camera_controller.camera.current = true
 	pass # Replace with function body.
 
 
